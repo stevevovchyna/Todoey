a993bfd10ba8013a8532b5c72554a5d5ba980790
stevevovchyna <steve.vovchyna@gmail.com>
2019-11-08 14:50:44 +0200

    add lists of reminders, add categoryTableView file, basic functionality is ready - adding, checking and searching for the todos

---
+++ Todoey/Controllers/CategoryViewController.swift
@@ -0,0 +1,109 @@
+//
+//  CategoryViewController.swift
+//  Todoey
+//
+//  Created by Steve Vovchyna on 08.11.2019.
+//  Copyright © 2019 Steve Vovchyna. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class CategoryViewController: UITableViewController {
+    
+    var categoryArray = [Categoria]()
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        loadItems()
+    }
+
+    //MARK: - TableView Datasource Methods
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "CategoryCell", for: indexPath)
+        let category = categoryArray[indexPath.row]
+        cell.textLabel?.text = category.name
+        return cell
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return categoryArray.count
+    }
+    
+    //MARK: - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        performSegue(withIdentifier: "goToItems", sender: self)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let destinationVC = segue.destination as! ToDoListViewController
+        if let indexPath = tableView.indexPathForSelectedRow {
+            destinationVC.selectedCategory = categoryArray[indexPath.row]
+        }
+    }
+    
+    //MARK: - Data Manipulation Methods
+    
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        
+        var textFieldValue = UITextField()
+        let emptyCategoryNameAlert = UIAlertController(title: "Empty Input", message: "Please enter some text", preferredStyle: .alert)
+        let emptyCategoryAlertAction = UIAlertAction(title: "Dismiss", style: .default) { (action) in
+            emptyCategoryNameAlert.dismiss(animated: true, completion: nil)
+        }
+        
+        emptyCategoryNameAlert.addAction(emptyCategoryAlertAction)
+        
+        
+        let alert = UIAlertController(title: "Add new Category", message: "", preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Category", style: .default) { (action) in
+            let range = NSRange(location: 0, length: textFieldValue.text!.utf16.count)
+            let regex = try! NSRegularExpression(pattern: #"^\s*$"#)
+            if regex.firstMatch(in: textFieldValue.text!, options: [], range: range) != nil {
+                self.present(emptyCategoryNameAlert, animated: true, completion: nil)
+            } else {
+                let newItem = Categoria(context: self.context)
+                newItem.name = textFieldValue.text!
+                self.categoryArray.append(newItem)
+                self.saveListData()
+            }
+        }
+        
+        let dismissAlert = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
+            alert.dismiss(animated: true, completion: nil)
+        }
+        
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create new Category"
+            textFieldValue = alertTextField
+        }
+         
+        alert.addAction(action)
+        alert.addAction(dismissAlert)
+        
+        present(alert, animated: true, completion: nil)
+        
+    }
+    
+    func saveListData() {
+        do {
+            try context.save()
+        } catch {
+            print("Error saving context: \(error)")
+        }
+        self.tableView.reloadData()
+    }
+    
+    func loadItems(with request: NSFetchRequest<Categoria> = Categoria.fetchRequest()) {
+        do {
+            categoryArray = try context.fetch(request)
+        } catch {
+            print("Error saving context: \(error)")
+        }
+        tableView.reloadData()
+    }
+
+}
--- Todoey/Controllers/ToDoListViewController.swift
+++ Todoey/Controllers/ToDoListViewController.swift
@@ -7,18 +7,20 @@
 //
 
 import UIKit
+import CoreData
 
 class ToDoListViewController: UITableViewController {
 
     var itemArray = [Item]()
-    
-     let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
+    var selectedCategory : Categoria?{
+        didSet{
+            loadItems()
+        }
+    }
+    let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
     
     override func viewDidLoad() {
-       
         super.viewDidLoad()
-        
-        loadItems()
     }
 
     //MARK: - Tableview Datasource Methods
@@ -41,7 +43,12 @@ class ToDoListViewController: UITableViewController {
     //MARK: - TableView Delegate Methods
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
         itemArray[indexPath.row].done = !itemArray[indexPath.row].done
+        
+//        context.delete(itemArray[indexPath.row])
+//        itemArray.remove(at: indexPath.row)
+        
         self.saveListData()
     }
     
@@ -50,79 +57,86 @@ class ToDoListViewController: UITableViewController {
     @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
         
         var textFieldValue = UITextField()
+        let emptyTodoAlert = UIAlertController(title: "Empty Input", message: "Please enter some text", preferredStyle: .alert)
+        let emptyTodoAlertAction = UIAlertAction(title: "Dismiss", style: .default) { (action) in
+            emptyTodoAlert.dismiss(animated: true, completion: nil)
+        }
+        
+        emptyTodoAlert.addAction(emptyTodoAlertAction)
+        
         
         let alert = UIAlertController(title: "Add new item", message: "", preferredStyle: .alert)
-        
         let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
-            let newItem = Item()
-            
-            if textFieldValue.text! == "" {
-               
-                newItem.title = "Empty to-do"
-                
-                self.itemArray.append(newItem)
-                
-                self.tableView.reloadData()
-                
+            let range = NSRange(location: 0, length: textFieldValue.text!.utf16.count)
+            let regex = try! NSRegularExpression(pattern: #"^\s*$"#)
+            if regex.firstMatch(in: textFieldValue.text!, options: [], range: range) != nil {
+                self.present(emptyTodoAlert, animated: true, completion: nil)
             } else {
-
+                let newItem = Item(context: self.context)
                 newItem.title = textFieldValue.text!
-
+                newItem.done = false
+                newItem.parentCategory = self.selectedCategory
                 self.itemArray.append(newItem)
-
                 self.saveListData()
-                
             }
-
+        }
+        
+        let dismissAlert = UIAlertAction(title: "Cancel", style: .cancel) { (action) in
+            alert.dismiss(animated: true, completion: nil)
         }
         
         alert.addTextField { (alertTextField) in
-
             alertTextField.placeholder = "Create new Item"
-
             textFieldValue = alertTextField
-
         }
-        
+         
         alert.addAction(action)
+        alert.addAction(dismissAlert)
         
         present(alert, animated: true, completion: nil)
     }
     
     func saveListData() {
-        
-        let encoder = PropertyListEncoder()
-        
         do {
-            
-            let data = try encoder.encode(itemArray)
-            
-            try data.write(to: dataFilePath!)
-
+            try context.save()
         } catch {
-
-            print("Error encoding array: \(error)")
-
+            print("Error saving context: \(error)")
         }
-
-        tableView.reloadData()
+        self.tableView.reloadData()
     }
     
-    func loadItems() {
-        if let data = try? Data(contentsOf: dataFilePath!) {
-            
-            let decoder = PropertyListDecoder()
-            
-            do {
-            
-                itemArray = try decoder.decode([Item].self, from: data)
+    func loadItems(with request: NSFetchRequest<Item> = Item.fetchRequest(), predicate: NSPredicate? = nil) {
+        let categoryPredicate = NSPredicate(format: "parentCategory.name MATCHES %@", selectedCategory!.name!)
+        
+        if let additionalPredicate = predicate {
+            request.predicate = NSCompoundPredicate(andPredicateWithSubpredicates: [categoryPredicate, additionalPredicate])
+        } else {
+            request.predicate = categoryPredicate
+        }
+        
+         do {
+            itemArray = try context.fetch(request)
+        } catch {
+            print("Error saving context: \(error)")
+        }
+        tableView.reloadData()
+    }
+}
 
-            } catch {
-
-                print("Error decoding array: \(error)")
-            
+extension ToDoListViewController : UISearchBarDelegate {
+    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
+        let request : NSFetchRequest<Item> = Item.fetchRequest()
+        let predicate = NSPredicate(format: "title CONTAINS[cd] %@", searchBar.text!)
+        request.sortDescriptors = [NSSortDescriptor(key: "title", ascending: true)]
+        loadItems(with: request, predicate: predicate)
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        if searchText == "" {
+            loadItems()
+            DispatchQueue.main.async {
+                searchBar.resignFirstResponder()
             }
         }
     }
 }
-
